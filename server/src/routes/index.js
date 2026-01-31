b4cd5dbd3f1b766c5a80945c313f9ff4380372a8
Mayank Mahajan <mayankmahajan9056@gmail.com>
2026-01-31 16:49:57 +0530

    feat(routes): add user routes to the main router

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import healthCheckRoutes from "./healthCheck.routes.js";
+import userRoutes from "./user.routes.js";
 
 const router = express.Router();
 
 router.use("/health", healthCheckRoutes);
+router.use("/users", userRoutes);
 
 export default router;
