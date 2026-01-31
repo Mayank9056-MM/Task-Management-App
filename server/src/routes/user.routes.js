d3e223171f6658d213f75863a6681f92fc9f491d
Mayank Mahajan <mayankmahajan9056@gmail.com>
2026-01-31 16:50:02 +0530

    feat(routes): create user routes for registration, login, logout, and profile retrieval

---
+++ server/src/routes/user.routes.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { getUserProfile, loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
+
+const router = express.Router();
+
+router.post("/register",registerUser);
+router.post("/login",loginUser);
+router.post("/logout",logoutUser);
+router.get("/profile",getUserProfile);
+
+export default router;
