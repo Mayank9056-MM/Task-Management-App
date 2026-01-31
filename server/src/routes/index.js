77f84d00e6d18a85f22e856bef237f0ba150dc59
Mayank Mahajan <mayankmahajan9056@gmail.com>
2026-01-31 18:03:53 +0530

    feat(routes): add task routes to main router for task management

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import healthCheckRoutes from "./healthCheck.routes.js";
 import userRoutes from "./user.routes.js";
+import taskRoutes from "./task.routes.js";
 
 const router = express.Router();
 
 router.use("/health", healthCheckRoutes);
 router.use("/users", userRoutes);
+router.use("/tasks", taskRoutes);
 
 export default router;
