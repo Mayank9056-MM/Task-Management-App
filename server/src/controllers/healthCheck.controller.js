cd2691e5e4ad653031b037c066a125bf671301d0
Mayank Mahajan <mayankmahajan9056@gmail.com>
2026-01-31 16:49:16 +0530

    refactor(health): move getReadyStateText function to healthCheck.controller.js

--- server/src/controllers/healthCheck.controller.js
+++ server/src/controllers/healthCheck.controller.js
@@ -1,4 +1,3 @@
-import { getReadyStateText } from "../helpers.js";
 import { getDBStatus } from "../db/db.js";
 
 export const healthCheck = async (req, res) => {
@@ -33,3 +32,19 @@ export const healthCheck = async (req, res) => {
     });
   }
 };
+
+function getReadyStateText(state) {
+  switch (state) {
+    case 0:
+      return "disconnected";
+    case 1:
+      return "connected";
+    case 2:
+      return "connecting";
+    case 3:
+      return "disconnecting";
+
+    default:
+      return "unknown";
+  }
+}
