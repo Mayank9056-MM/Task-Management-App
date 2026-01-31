5b364eb90887609b04157458c68158ab64d0433b
Mayank Mahajan <mayankmahajan9056@gmail.com>
2026-01-31 16:59:26 +0530

    feat(routes): add authentication middleware to logout and profile routes

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { getUserProfile, loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
+import { verifyAuth } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/register",registerUser);
 router.post("/login",loginUser);
-router.post("/logout",logoutUser);
-router.get("/profile",getUserProfile);
+router.post("/logout",verifyAuth,logoutUser);
+router.get("/profile",verifyAuth,getUserProfile);
 
 export default router;
