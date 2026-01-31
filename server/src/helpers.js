f15fa08ed84352a1b4c36919cdfa6d931dabc8a5
Mayank Mahajan <mayankmahajan9056@gmail.com>
2026-01-31 15:50:34 +0530

    feat(helpers): add getReadyStateText function for state representation

---
+++ server/src/helpers.js
@@ -0,0 +1,15 @@
+export function getReadyStateText(state) {
+    switch (state) {
+      case 0:
+        return "disconnected";
+      case 1:
+        return "connected";
+      case 2:
+        return "connecting";
+      case 3:
+        return "disconnecting";
+
+      default:
+        return "unknown";
+    }
+  }
