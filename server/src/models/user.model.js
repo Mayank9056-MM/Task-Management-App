513eb6599b3c213d5918fc07deaad7c139081170
Mayank Mahajan <mayankmahajan9056@gmail.com>
2026-01-31 16:49:35 +0530

    feat(user): create user model with schema, password hashing, and token generation

---
+++ server/src/models/user.model.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      lowerCase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return;
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY },
+  );
+};
+
+export const User = mongoose.model("User", userSchema);
