2dc4e0b674492e23f4f9a93310533983d5c107f4
Mayank Mahajan <mayankmahajan9056@gmail.com>
2026-01-31 16:49:42 +0530

    feat(user): implement user authentication and profile management functions

---
+++ server/src/controllers/user.controller.js
@@ -0,0 +1,116 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+
+// helper functions
+const generateAccessToken = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+
+    if (!user) {
+      throw new ApiError(400, "user not found please register");
+    }
+
+    const accessToken = user.generateAccessToken();
+
+    return { accessToken };
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "something went wrong while generating access token",
+    );
+  }
+};
+
+// main functions
+export const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if ([name, email, password].some((field) => field?.trim() === "")) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser) {
+    throw new ApiError(409, "User with email already exists");
+  }
+
+  const user = await User.create({
+    email,
+    password,
+    name: name.toLowerCase(),
+  });
+
+  const createdUser = await User.findById(user._id).select("-password");
+
+  if (!createdUser) {
+    throw new ApiError(500, "User creation failed");
+  }
+
+  res
+    .status(201)
+    .json(new ApiResponse(201, createdUser, "User registered successfully"));
+});
+
+export const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  if ([email, password].some((field) => field?.trim() === "")) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  const isPasswordValid = await user.isPasswordCorrect(password);
+
+  if (!isPasswordValid) {
+    throw new ApiError(401, "Invalid credentials");
+  }
+
+  const userData = await User.findById(user._id).select("-password");
+
+  const { accessToken } = await generateAccessToken(user._id);
+
+  const options = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
+  };
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .json(new ApiResponse(200, userData, "User logged in successfully"));
+});
+
+export const getUserProfile = asyncHandler(async (req, res) => {
+  const userId = req.user._id;
+
+  const user = await User.findById(userId).select("-password");
+
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, user, "User profile fetched successfully"));
+});
+
+export const logoutUser = asyncHandler(async (req, res) => {
+  const options = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+  };
+
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .json(new ApiResponse(200, {}, "User logged out successfully"));
+});
